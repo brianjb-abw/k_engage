b3cc890ec0492f61db98355e41a28a576f2a38ae
brianjb-abw <brianjb-tek@proton.mem>
2026-05-23 14:50:43 -0400

    loaded card info, mod tr list and card to list report all info

--- app.py
+++ app.py
@@ -29,16 +29,15 @@ def get_token():
 
     response = requests.post(url, headers=headers, json=payload)
     data = response.json()
-    print(data)
     new_sess_token = data["data"]["createSessionToken"]
-    print(f"new token {new_sess_token}")
+    print(f"new token:  {new_sess_token}")
 
     os.environ["BEARER"] = new_sess_token
-    print(f"new env var 'BEARER' {os.environ["BEARER"]}")
-    dotenv.set_key(f_dotenv, "BEARER", os.environ["BEARER"])
+    dotenv.set_key(f_dotenv, "BEARER", new_sess_token)
     dotenv.load_dotenv(override=True)
+    BEARER = new_sess_token
 
-    headers["Authorization"] = data["data"]["createSessionToken"]
+    headers["Authorization"] = f"Bearer {BEARER}"
     print(f"headers {headers}")
 
 def get_balance():
@@ -59,9 +58,10 @@ def get_cards():
 
     print(f"\n\n--- CARDS ---\n")
     for card in cards:
-        if card['status'] != "TERMINATED":
-            print(f"{card['lastFour']}\t\t{card['name']:12}\t{card['status']}\n")
-            card_ct += 1
+        # if card['status'] != "TERMINATED":
+        print(card)
+        
+        card_ct += 1
 
     print(f"\nrun complete: {card_ct} cards processed")
 
@@ -71,15 +71,10 @@ def get_trans():
     data = response.json()
     t_list = data['data']['transactions']
 
-    print(f"\n\n*** TRANSACTIONS ***\n--------------------\n")
+    print(f"\n\n--- TRANSACTIONS ---\n")
     for t in t_list:
-        print(f"{t['transactionDate']}")
-        if t['card']:
-            print(f"  card:    {t['merchantName']} - {cards[t['card']['id']]['last_4']}")
-        else:
-            print(f"  card:    {t['merchantName']}")
-        print(f"  amt:     {t['amount']:>10,.2f}")
-        print(f"  status:  {t['transactionStatus'].lower()}\n")
+        print(t)
+
 
 # MAIN ROUTINE
 
@@ -95,7 +90,6 @@ while True:
         print(f"bearer var: {BEARER}")
     elif curr_task == "B":
         get_balance()
-        print(cards["1234"])
     elif curr_task == "CL":
         get_cards()
     elif curr_task == "XL":
