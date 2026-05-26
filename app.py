724812551e5b9b292ab380f78d1a6b3de8e52624
brianjb-abw <brianjb-tek@proton.me>
2026-05-26 04:54:34 -0400

    added token exp tracking

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import requests
 import dotenv
+from datetime import datetime, timedelta
 from k_payloads import k_payloads 
-# from cards import cards
+from cards import cards
 
 
 f_dotenv = dotenv.find_dotenv()
@@ -11,17 +12,21 @@ dotenv.load_dotenv(override=True)
 p_url = "https://graphql.klutchcard.com/graphql"
 sb_url = "https://sandbox.klutchcard.com/graphql"
 
-# ----- ACTIVE URL ----- #
+# ----- ACTIVE URL -----
 url = p_url
 
+# ----- STD HEADERS -----
 headers = {
     'Content-Type': 'application/json',
     'Authorization': f"Bearer {os.getenv('BEARER')}"
 }
 
+# ----- TOKEN EXP -----
+token_exp = datetime.strptime(os.getenv('TOKEN_EXP'), '%Y-%m-%d %H:%M:%S.%f')
+
 
 def get_token():
-
+    print("running get_token")
     headers.pop('Authorization')
 
     payload = k_payloads['get_token']
@@ -31,11 +36,17 @@ def get_token():
     }
 
     response = requests.post(url, headers=headers, json=payload)
+    print(response)
     data = response.json()
+
     new_sess_token = data['data']['createSessionToken']
     print(f"new token:  {new_sess_token}")  # <<<<<< REMOVE
 
+    token_exp = datetime.now() + timedelta(minutes=50)
+    print(datetime.strftime(token_exp, '%Y-%m-%d %H:%M:%S.%f'))
+
     dotenv.set_key(f_dotenv, 'BEARER', new_sess_token)
+    dotenv.set_key(f_dotenv, 'TOKEN_EXP', datetime.strftime(token_exp, '%Y-%m-%d %H:%M:%S.%f'))
     dotenv.load_dotenv(override=True)
 
     headers['Authorization'] = f"Bearer {os.getenv('BEARER')}"
@@ -89,10 +100,12 @@ def main():
         curr_task = input(prompt)
         curr_task = curr_task.upper()
 
+        if curr_task != "T":
+            if ((token_exp - datetime.now()).total_seconds())/60 < 10:
+                get_token()
+        
         if curr_task == "T":
             get_token()
-            BEARER = os.getenv("BEARER")
-            print(f"bearer var: {BEARER}")
         elif curr_task == "B":
             get_balance()
         elif curr_task == "CL":
